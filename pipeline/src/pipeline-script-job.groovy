253ca4f20da1d348ad02a65efd4b43b2f248b657
mk <mk@DESKTOP-IATQN43.localdomain>
2019-04-05 09:32:08 +0800

    添加一句打印代码

--- pipeline/src/pipeline-script-job.groovy
+++ pipeline/src/pipeline-script-job.groovy
@@ -11,6 +11,7 @@ pipeline{
 		stage("git checkout") {
 			steps{
 				script {
+					println "welcome yw"
 					checkout([$class: 'GitSCM', branches: [[name: '*/master']],
 						userRemoteConfigs: [[credentialsId: '6f4fa66c-eb02-46dc-a4b3-3a232be5ef6e', 
 							url: 'https://github.com/Anthonyliu86/HelloWorld.git']]])
